7095b598c0e88264df096c2a15c6790b432635f5
Enci21 <eni.hegedus.95@gmail.com>
2019-12-16 17:34:19 +0100

    Start to create RegistrationService

--- src/main/java/com/doggoesinthecity/doggoesapp/service/RegistrationService.java
+++ src/main/java/com/doggoesinthecity/doggoesapp/service/RegistrationService.java
@@ -1,4 +1,13 @@
 package com.doggoesinthecity.doggoesapp.service;
 
+import com.doggoesinthecity.doggoesapp.model.RegisterForm;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Service
 public class RegistrationService {
+
+    public void newUserRegistration(@RequestBody RegisterForm registerForm){
+
+    }
 }
