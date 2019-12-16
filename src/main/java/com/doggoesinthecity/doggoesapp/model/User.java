0496c4ce757a54cdb886b1cf616194a78216c0e5
Enci21 <eni.hegedus.95@gmail.com>
2019-12-16 17:22:18 +0100

    Add fields to User

--- src/main/java/com/doggoesinthecity/doggoesapp/model/User.java
+++ src/main/java/com/doggoesinthecity/doggoesapp/model/User.java
@@ -1,4 +1,28 @@
 package com.doggoesinthecity.doggoesapp.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Builder
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    Long id;
+
+    String username;
+    String email;
+    String password;
 }
