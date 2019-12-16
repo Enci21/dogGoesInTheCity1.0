832c106fe00c407d13d57202f0b6851baea7b780
Enci21 <eni.hegedus.95@gmail.com>
2019-12-16 17:11:24 +0100

    Initial commit

---
+++ src/main/java/com/doggoesinthecity/doggoesapp/DoggoesappApplication.java
@@ -0,0 +1,13 @@
+package com.doggoesinthecity.doggoesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoggoesappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoggoesappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/doggoesinthecity/doggoesapp/DoggoesappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doggoesinthecity.doggoesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoggoesappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
