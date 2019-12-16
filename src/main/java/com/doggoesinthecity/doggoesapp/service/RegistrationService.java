2268cd7a4dd721a43bf27068011869cbdfe22918
Enci21 <eni.hegedus.95@gmail.com>
2019-12-16 17:46:59 +0100

    Implement newUserRegistration method

--- src/main/java/com/doggoesinthecity/doggoesapp/service/RegistrationService.java
+++ src/main/java/com/doggoesinthecity/doggoesapp/service/RegistrationService.java
@@ -1,13 +1,25 @@
 package com.doggoesinthecity.doggoesapp.service;
 
 import com.doggoesinthecity.doggoesapp.model.RegisterForm;
+import com.doggoesinthecity.doggoesapp.model.User;
+import com.doggoesinthecity.doggoesapp.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @Service
 public class RegistrationService {
 
-    public void newUserRegistration(@RequestBody RegisterForm registerForm){
+    @Autowired
+    UserRepository userRepository;
 
+    public void newUserRegistration(@RequestBody RegisterForm registerForm){
+        User newUser = User.builder()
+                .email(registerForm.getEmail())
+                .password(registerForm.getPassword())
+                .username(registerForm.getUsername())
+                .build();
+
+        userRepository.save(newUser);
     }
 }
