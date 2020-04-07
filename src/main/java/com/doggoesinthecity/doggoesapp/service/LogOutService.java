2ba655a680a04fd01969f15383bef649aed6641d
Enci21 <eni.hegedus.95@gmail.com>
2020-04-07 16:05:52 +0200

    Overwrite the existing cookie

--- src/main/java/com/doggoesinthecity/doggoesapp/service/LogOutService.java
+++ src/main/java/com/doggoesinthecity/doggoesapp/service/LogOutService.java
@@ -1,19 +1,22 @@
 package com.doggoesinthecity.doggoesapp.service;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 
 @Service
 public class LogOutService {
 
-    public void logOut(HttpServletRequest req) {
-        Cookie[] cookies = req.getCookies();
-        for (Cookie c : cookies) {
-            if (c.getName().equals("token")) {
-                c.setValue("");
-            }
-        }
+    public ResponseEntity logOut(HttpServletRequest req, HttpServletResponse response) {
+        Cookie cookie = new Cookie("token", null);
+        cookie.setMaxAge(60 * 60 * 24);
+        cookie.setHttpOnly(true);
+        cookie.setPath("/");
+        response.addCookie(cookie);
+        return ResponseEntity.ok("");
     }
 }
