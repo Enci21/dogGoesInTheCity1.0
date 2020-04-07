6bbe40744615ba8f4b5ed42cd362c5f68b94b3b2
Enci21 <eni.hegedus.95@gmail.com>
2020-04-07 16:06:34 +0200

    Send back response entity

--- src/main/java/com/doggoesinthecity/doggoesapp/controller/LogOutController.java
+++ src/main/java/com/doggoesinthecity/doggoesapp/controller/LogOutController.java
@@ -2,21 +2,26 @@ package com.doggoesinthecity.doggoesapp.controller;
 
 import com.doggoesinthecity.doggoesapp.service.LogOutService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @RestController
 @CrossOrigin
+@RequestMapping("/log")
 public class LogOutController {
 
     @Autowired
     LogOutService logOutService;
 
-    @PutMapping("/logout")
-    public void logOut(HttpServletRequest req) {
-        logOutService.logOut(req);
+    @GetMapping("/out")
+    public ResponseEntity logOut(HttpServletRequest req, HttpServletResponse response) {
+        logOutService.logOut(req, response);
+        return ResponseEntity.ok("");
     }
 }
