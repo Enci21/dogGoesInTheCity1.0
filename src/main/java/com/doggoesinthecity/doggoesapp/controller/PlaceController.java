54b4dd330919386cc093c3164ec58bc22de9a8d3
Enci21 <eni.hegedus.95@gmail.com>
2020-04-08 15:06:15 +0200

    Edit endpoint

--- src/main/java/com/doggoesinthecity/doggoesapp/controller/PlaceController.java
+++ src/main/java/com/doggoesinthecity/doggoesapp/controller/PlaceController.java
@@ -3,19 +3,17 @@ package com.doggoesinthecity.doggoesapp.controller;
 import com.doggoesinthecity.doggoesapp.model.Place;
 import com.doggoesinthecity.doggoesapp.service.PlaceService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin
+@RequestMapping("/place")
 public class PlaceController {
 
     @Autowired
     PlaceService placeService;
 
-    @PostMapping
+    @PostMapping("/new")
     public void addNewPlace(@RequestBody Place data) {
         placeService.addPlace(data);
     }
