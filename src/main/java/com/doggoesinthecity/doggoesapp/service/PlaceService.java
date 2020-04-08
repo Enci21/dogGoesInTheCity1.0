b8a762aae3c1d9eacc7b7a12c546beee73d4cb0c
Enci21 <eni.hegedus.95@gmail.com>
2020-04-08 15:05:09 +0200

    Create PlaceService

---
+++ src/main/java/com/doggoesinthecity/doggoesapp/service/PlaceService.java
@@ -0,0 +1,26 @@
+package com.doggoesinthecity.doggoesapp.service;
+
+import com.doggoesinthecity.doggoesapp.model.Place;
+import com.doggoesinthecity.doggoesapp.repositories.PlaceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PlaceService {
+
+    @Autowired
+    PlaceRepository placeRepository;
+
+
+    public void addPlace(Place data) {
+
+        Place place = Place.builder()
+                .address(data.getAddress())
+                .name(data.getName())
+                .number(data.getNumber())
+                .openingHours(data.getOpeningHours())
+                .rating(data.getRating())
+                .build();
+        placeRepository.save(place);
+    }
+}
